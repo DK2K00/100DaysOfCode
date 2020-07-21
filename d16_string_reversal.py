564144cd213d94b83a4bc578bc831b64ff7a433b
Danush Kumar <52857686+DK2K00@users.noreply.github.com>
2020-07-21 16:58:03 +0530

    Day 16 of code

--- d16_string_reversal.py
+++ d16_string_reversal.py
@@ -1,10 +1,13 @@
+#Function to reverse string using recursion
 def reverse(s):
 
+    #To determine length of string
     length = len(s)
 
     if(length <= 1):
         return(s)
     
+    #Recursion
     return(s[length-1] + reverse(s[0:length-1]))
 
 reverse("helloworld")
