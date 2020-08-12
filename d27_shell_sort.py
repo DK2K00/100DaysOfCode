34766f13b8f65bdbf73d583d77ad75790f3ea112
Danush Kumar <52857686+DK2K00@users.noreply.github.com>
2020-08-12 20:32:32 +0530

    Day 27 of code

---
+++ d27_shell_sort.py
@@ -0,0 +1,26 @@
+#Function to perform shell sort
+def shell(arr):
+    sublist = int(len(arr)/2)
+
+    #To create gaps to perform sorting
+    while(sublist > 0):
+        for start in range(sublist):
+            gap_insertion(arr,start,sublist)
+
+        sublist = int(sublist / 2)
+
+#To perform insertion sort for repective sublist
+def gap_insertion(arr,start,gap):
+    for i in range(start+gap, len(arr), gap):
+        currentVal = arr[i]
+        pos = i
+
+        while(pos >= gap and arr[pos-gap] > currentVal):
+            arr[pos] = arr[pos-gap]
+            pos = pos - gap
+
+        arr[pos] = currentVal
+
+arr = [2,1,4,3,5,7,6]
+shell(arr)
+print(arr)
