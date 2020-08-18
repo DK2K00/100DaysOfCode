1a7175238435dafcd28f07447885897ae0fbcfc0
Danush Kumar <52857686+DK2K00@users.noreply.github.com>
2020-08-18 22:31:24 +0530

    Day 30 of code

---
+++ d30_bfs.py
@@ -0,0 +1,23 @@
+#Test graph
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+#Function to perform bfs
+def bfs(graph,start):
+    visited, queue = set(), [start]
+
+    while(queue):
+        vertex = queue.pop(0)
+
+        if(vertex not in visited):
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    
+    return visited
+
+#Testing
+bfs(graph, 'A')
